a55db467384d94416e072a7c7e5248c37f6cf8fa
Matthew Epstein <me382@cornell.edu>
2018-09-18 17:54:27 -0400

    added non-working logic to bearStatus in reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -5,12 +5,31 @@ const initialState = {
         tagColor2 : '#ffffff',
         tagColor3 : '#ff0000',
     },
+
     bearStatus : 'neutral',
 }
 
 //function to update the bear's status
 function recalculateBearStatus(taskArray) {
-	return 'neutral';
+    const focusTaskArray = function makeFocusTaskArray = () => {
+        /* a function that parses mainTaskArray for tasks that are in focus
+         * and returns an array of those focus tasks. Will remove finished 
+         * entries only at end of day. */
+        //TODO
+    }
+    if (focusTaskArray.isComplete) {
+        return 'happy';
+    } else if (focusTaskArray.justFinishedTask) {
+        return 'has fish';
+    } else if (focusTaskArray.howComplete < .1) {
+        return 'hungry';
+    } else if (focusTaskArray.hasOverDueTask == 1) {
+        return 'hibernating';
+    } else if (focusTaskArray.isAllOverDue || focusTaskArray.hasOverDueTask >= 1) {
+        return 'leaving';
+    } else {
+        return 'neutral';
+    }
 }
 
 const rootReducer = (state = initialState, action) => {
