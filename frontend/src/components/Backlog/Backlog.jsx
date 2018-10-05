c0aaed21da95d792b2fe70aa40b702adf94f757f
Sam <sam@developersam.com>
2018-10-05 10:53:40 -0400

    Optimize performance of processing data for backlog

--- frontend/src/components/Backlog/Backlog.jsx
+++ frontend/src/components/Backlog/Backlog.jsx
@@ -1,37 +1,52 @@
+// @flow
+
 import * as React from 'react';
 import { connect } from 'react-redux';
-import type { OneDayTask, SimpleTask } from './types';
+import type { OneDayTask } from './types';
 import BacklogDay from './BacklogDay';
 import type { State, Task } from '../../store/store-types';
 import styles from './Backlog.css';
 
-const mapStateToProps = (state: State) => {
+type Props = {| days: OneDayTask[] |};
+
+const mapStateToProps = (state: State): Props => {
   const days: OneDayTask[] = [];
   const aMonthLater = new Date();
+  // build a map from day to its tasks for faster access
+  const day2TaskMap = (() => {
+    const map: Map<string, Task[]> = new Map();
+    state.mainTaskArray.forEach((task) => {
+      const dateString = task.date.toDateString();
+      const tasksArrOpt = map.get(dateString);
+      if (tasksArrOpt == null) {
+        map.set(dateString, [task]);
+      } else {
+        tasksArrOpt.push(task);
+      }
+    });
+    return map;
+  })();
+  // display only events for next 30 days
   aMonthLater.setDate(aMonthLater.getDate() + 30);
   for (let d = new Date(); d <= aMonthLater; d.setDate(d.getDate() + 1)) {
     const date = new Date(d);
-    const tasks: SimpleTask[] = state.mainTaskArray
-      .filter((task: Task) => {
-        const tDate = task.date;
-        return tDate.getFullYear() === date.getFullYear()
-          && tDate.getMonth() === date.getMonth() && tDate.getDate() === date.getDate();
-      })
-      .map(({ name, tag }: Task) => ({ name, color: state.tagColorPicker[tag] }));
+    const tasksOnThisDay = day2TaskMap.get(date.toDateString()) || [];
+    const tasks = tasksOnThisDay.map(({ name, tag }) => ({
+      name,
+      color: state.tagColorPicker[tag],
+    }));
     days.push({ date, tasks });
   }
   return { days };
 };
 
-type Props = {| days: OneDayTask[] |};
-
 function Backlog({ days }: Props) {
-  const renderDay = (day: OneDayTask) => (
-    <BacklogDay key={day.date.toDateString()} {...day} />
-  );
+  const renderDay = (day: OneDayTask) => <BacklogDay key={day.date.toDateString()} {...day} />;
   return <div className={styles.Backlog}>{days.map(renderDay)}</div>;
 }
 
-const ConnectedBackLog = connect(mapStateToProps, null)(Backlog);
-
+const ConnectedBackLog = connect(
+  mapStateToProps,
+  null,
+)(Backlog);
 export default ConnectedBackLog;
