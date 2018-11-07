0c2c83260fb025daf84856c8a4133190ec539913
Sam <sam@developersam.com>
2018-11-07 11:10:53 -0500

    Improve code quality of class picker

--- frontend/src/components/ClassPicker/ClassPickerItem.jsx
+++ frontend/src/components/ClassPicker/ClassPickerItem.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+// @flow strict
 
+import * as React from 'react';
+import type { Node } from 'react';
 
-function ClassPickerItem({ classColor, classTitle, changeCallback }) {
+type Props = {|
+  +color: string;
+  +title: string;
+  +onChange: (SyntheticEvent<HTMLInputElement>) => void;
+|};
+
+/**
+ * One item in the class picker.
+ *
+ * @param {string} color the color of the class.
+ * @param {string} title the title of the class.
+ * @param {function} onChange the function to call when choice changed.
+ * @return {Node} the rendered item.
+ * @constructor
+ */
+export default function ClassPickerItem({ color, title, onChange }: Props): Node {
   return (
-    <li style={{ '--custom-color': classColor }}>
-      <input data-color={classColor} data-class-title={classTitle} onClick={changeCallback} type="checkbox" />
-      {classTitle}
+    <li style={{ '--custom-color': color }}>
+      <input
+        data-color={color}
+        data-class-title={title}
+        onClick={onChange}
+        type="checkbox"
+      />
+      {title}
     </li>
   );
 }
-
-export default ClassPickerItem;
