7ccb10fc4b785559f340a4b3088da969400b57af
Sam <sam@developersam.com>
2018-11-11 23:05:31 -0500

    Update docs for react-redux-util

--- frontend/src/store/react-redux-util.js
+++ frontend/src/store/react-redux-util.js
@@ -7,17 +7,11 @@ import type { State } from './store-types';
 /**
  * A connect function for react-redux that just uses the normal mapStateToProps.
  *
- * @param mapStateToProps the normal mapStateToProps function.
- * @return {*} the connect function that connects a react component.
- */
-export function simpleConnect<P: Object, OP: Object, SP: Object>(
-  mapStateToProps: (state: State) => SP,
-): (ComponentType<P>) => (ComponentType<OP>) {
-  return connect<ComponentType<P>, _, {}, _, _, OP, _>(mapStateToProps, {});
-}
-
-/**
- * A connect function for react-redux that just uses the normal mapStateToProps.
+ * Type Parameters:
+ * P: all the props.
+ * OP: own props. The props that the user of the component must give.
+ * SP: subscribed props. The props that are derived from redux store state.
+ * MP: the props of a collection of actions to be dispatched.
  *
  * @param mapStateToProps the normal mapStateToProps function.
  * @param actionCreators the action creator used to bind actions.
@@ -28,4 +22,21 @@ export function fullConnect<P: Object, OP: Object, SP: Object, MP: Object>(
   actionCreators: MP,
 ): (ComponentType<P>) => (ComponentType<OP>) {
   return connect<ComponentType<P>, _, MP, _, _, OP, _>(mapStateToProps, actionCreators);
+}
+
+/**
+ * A connect function for react-redux that just uses the normal mapStateToProps.
+ *
+ * Type Parameters:
+ * P: all the props.
+ * OP: own props. The props that the user of the component must give.
+ * SP: subscribed props. The props that are derived from redux store state.
+ *
+ * @param mapStateToProps the normal mapStateToProps function.
+ * @return {*} the connect function that connects a react component.
+ */
+export function simpleConnect<P: Object, OP: Object, SP: Object>(
+  mapStateToProps: (state: State) => SP,
+): (ComponentType<P>) => (ComponentType<OP>) {
+  return fullConnect(mapStateToProps, {});
 }
