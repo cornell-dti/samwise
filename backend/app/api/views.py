3a8562166d21b24872ae4651dbd3f8498fe47528
Gautam Mekkat <Blue9@users.noreply.github.com>
2018-11-18 14:03:33 -0500

    Removed test endpoint

--- backend/app/api/views.py
+++ backend/app/api/views.py
@@ -31,55 +31,6 @@ def login():
         return auth.html
 
 
-@api.route('/test', methods=['GET'])
-def test_auth():
-    return '''
-    <script src="https://www.gstatic.com/firebasejs/5.5.8/firebase.js"></script>
-    <script>
-      var config = {
-        apiKey: "AIzaSyBrnR-ai3ZQrr3aYnezDZTZdw9e2TWTRtc",
-        authDomain: "dti-samwise.firebaseapp.com",
-        databaseURL: "https://dti-samwise.firebaseio.com",
-        projectId: "dti-samwise",
-        storageBucket: "dti-samwise.appspot.com",
-        messagingSenderId: "114434220691"
-      };
-      firebase.initializeApp(config);
-      
-      var provider = new firebase.auth.GoogleAuthProvider();
-      provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-      firebase.auth().useDeviceLanguage();
-      provider.setCustomParameters({
-        'login_hint': 'user@example.com'
-      });
-
-      firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          firebase.auth().currentUser.getIdToken(true).then(function(idToken) {
-            fetch('http://localhost:5000/api/tags/all?token=' + idToken).then(function(response) {
-              response.json().then(function (data) {
-                alert(JSON.stringify(data));
-              });
-            });
-          }).catch(function(error) {
-            console.log(error);
-          });  
-        } else {
-          firebase.auth().signInWithRedirect(provider);
-          firebase.auth().getRedirectResult().then(function(result) {
-            var token = result.credential.accessToken;
-            var user = result.user;
-          }).catch(function(error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            var email = error.email;
-            var credential = error.credential;
-          });
-        }
-      });
-    </script>
-    '''
-
 
 @api.route('/tags/all', methods=['GET'])
 def get_tags():
