597334624e295c983721bc38783cc52aafc41524
Gautam Mekkat <Blue9@users.noreply.github.com>
2018-11-18 14:59:11 -0500

    Added mark task, delete task

--- backend/app/api/views.py
+++ backend/app/api/views.py
@@ -14,6 +14,7 @@ def get_user_id(firebase_id_token):
     except ValueError:
         return
 
+
 @api.route('/', methods=['GET'])
 def index():
     return jsonify(status='success')
@@ -29,6 +30,7 @@ def login():
         return redirect(redirect_url)
     else:
         return auth.html
+
 
 @api.route('/tags/new', methods=['POST'])
 def new_tag():
@@ -50,8 +52,9 @@ def new_tag():
         "completed": False,
     }
     """
-    # TODO Use current user id instead of hardcoded 1
-    user_id = 1
+    user_id = get_user_id(session.get('token'))
+    if not user_id:
+        return redirect(url_for('api.login', redirect=request.path))
     data = request.get_json(force=True)
     tag_name = data['name']
     color = data['color']
@@ -62,13 +65,13 @@ def new_tag():
     db.session.commit()
     return jsonify(created=util.sqlalchemy_object_to_dict(tag))
 
+
 @api.route('/tags/all', methods=['GET'])
 def get_tags():
     """
     Returns all tags.
 
     Output format:
-
     List of tags in form:
 
     {
@@ -79,7 +82,6 @@ def get_tags():
         "completed": False,
     }
     """
-    # TODO Use current user id instead of hardcoded 1
     user_id = get_user_id(session.get('token'))
     if not user_id:
         return redirect(url_for('api.login', redirect=request.path))
@@ -87,9 +89,21 @@ def get_tags():
     tags_json = util.table_to_json(tags)
     return jsonify(tags_json)
 
+
+@api.route('/tags/<tag_id>/delete', methods=['PUT'])
+def delete_tag(tag_id):
+    user_id = get_user_id(session.get('token'))
+    if not user_id:
+        return redirect(url_for('api.login', redirect=request.path))
+    tag = Tag.query.filter(Tag.tag_id == tag_id).filter(Tag.user_id == user_id).first()
+    if tag is None:
+        return jsonify(error='error. no tag with id {} exists for this user.'.format(tag_id)), 404
+    tag.deleted = True
+    return jsonify(status='success')
+
+
 @api.route('/tags/<tag_id>/tasks/new', methods=['POST'])
 def new_task(tag_id):
-
     """
     Creates a new task.
     {
@@ -106,28 +120,30 @@ def new_task(tag_id):
         "end_date": yyyy-mm-dd hh:mm:ss,
         "tag_id": id,
         "parent_task": parent id,
-        "in_focus": False,
         "_order": order,
         "completed": False
     }
     """
-    user_id = 1
+    user_id = get_user_id(session.get('token'))
+    if not user_id:
+        return redirect(url_for('api.login', redirect=request.path))
     data = request.get_json(force=True)
     content = data['content']
     start_date = data['start_date']
     end_date = data['end_date']
     parent_task = data.get('parent_task', None)
-    tag = Tag.query.filter(Tag.tag_id == tag_id).filter(Tag.user_id == user_id).all()
-    if len(tag) == 0:
-        return jsonify(status='error. no tag with id {} exists for this user.'.format(tag_id))
+    tag = Tag.query.filter(Tag.tag_id == tag_id).filter(Tag.user_id == user_id).first()
+    if tag is None:
+        return jsonify(error='error. no tag with id {} exists for this user.'.format(tag_id)), 404
     last_task = Task.query.filter(Task.tag_id == Tag.tag_id).filter(Tag.tag_id == tag_id).filter(
         Tag.user_id == user_id).order_by(Task._order.desc()).first()
     order = last_task._order + 1 if last_task else 0
     new_task = Task(content=content, start_date=start_date, end_date=end_date, tag_id=tag_id, parent_task=parent_task,
-                    in_focus=False, _order=order, completed=False)
+                    _order=order, completed=False)
     db.session.add(new_task)
     db.session.commit()
     return jsonify(created=util.sqlalchemy_object_to_dict(new_task))
+
 
 @api.route('/tags/<tag_id>/tasks/all', methods=['GET'])
 def get_tasks(tag_id):
@@ -149,8 +165,9 @@ def get_tasks(tag_id):
         "completed": False
     }
     """
-    # TODO Use current user id instead of hardcoded 1
-    user_id = 1
+    user_id = get_user_id(session.get('token'))
+    if not user_id:
+        return redirect(url_for('api.login', redirect=request.path))
     tasks = Task.query.filter(Task.tag_id == Tag.tag_id).filter(Tag.user_id == user_id).filter(
         Tag.tag_id == tag_id).all()
     return jsonify(util.table_to_json(tasks))
@@ -176,8 +193,54 @@ def get_tasks_in_focus():
         "completed": False
     }
     """
-    # TODO Use current user id instead of hardcoded 1
-    user_id = 1
+    user_id = get_user_id(session.get('token'))
+    if not user_id:
+        return redirect(url_for('api.login', redirect=request.path))
     tasks = Task.query.filter(Task.user_id == user_id).filter(Task.in_focus == True).all()
     tasks_json = util.table_to_json(tasks)
     return jsonify(tasks_json)
+
+
+@api.route('/tasks/<task_id>/mark', methods=['PUT'])
+def mark_task_complete(task_id):
+    """
+    Mark the given task as complete/incomplete.
+
+    Input format:
+    {"complete": true|false}
+
+    Output format:
+    {"status": "success"} if succeeded.
+    {"error": error message} if failed.
+    {"
+    """
+    user_id = get_user_id(session.get('token'))
+    if not user_id:
+        return redirect(url_for('api.login', redirect=request.path))
+    data = request.get_json(force=True)
+    if not data or not ('completed' in data) or not (type(data['completed']) == bool):
+        return jsonify(error='param completed missing or of wrong type'), 400
+    completed = data['completed']
+    task = Task.query.filter(Task.task_id == task_id).filter(Task.user_id == user_id).first()
+    if task is None:
+        return jsonify(error='error. no task with id {} exists for this user.'.format(task_id)), 404
+    task.completed = completed
+
+
+@api.route('/tasks/<task_id>/delete', methods=['PUT'])
+def delete_task(task_id):
+    """
+    Delete the given task.
+
+    Output format:
+    {"status": "success"} if succeeded.
+    {"error": error message} if failed.
+    """
+    user_id = get_user_id(session.get('token'))
+    if not user_id:
+        return redirect(url_for('api.login', redirect=request.path))
+    task = Task.query.filter(Task.task_id == task_id).filter(Task.user_id == user_id).first()
+    if task is None:
+        return jsonify(status='error. no task with id {} exists for this user.'.format(task_id)), 404
+    task.deleted = True
+    return jsonify(status='success')
