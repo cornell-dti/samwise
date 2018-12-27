c93cb631f8734e4b41dd77f302f62352f851f68c
Sam <sam@developersam.com>
2018-12-27 15:15:57 +0800

    Update type definition according to new react-redux typing

--- frontend/src/store/react-redux-util.js
+++ frontend/src/store/react-redux-util.js
@@ -7,15 +7,13 @@ import type { State } from './store-types';
 /**
  * The connected component type.
  */
-// flowlint-next-line unclear-type:off
-export type ConnectedComponent<Config: Object, RP: Object = {||}> =
-  AbstractComponent<$Diff<Config, RP>, void>;
+export type ConnectedComponent<-C, -RP = {||}> =
+  AbstractComponent<$Diff<C, RP>, void>;
 
 /**
  * The utility type to obtain own props in fullConnect.
  */
-// flowlint-next-line unclear-type:off
-type OwnProp<C: Object, RSP: Object, MDP: Object> = $Diff<C, {| ...RSP; ...MDP |}>;
+type OwnProp<-C, -RSP, -MDP> = $Diff<C, {| ...RSP; ...MDP |}>;
 
 /**
  * A connect function for react-redux that uses the normal mapStateToProps and actionCreators.
@@ -24,7 +22,7 @@ type OwnProp<C: Object, RSP: Object, MDP: Object> = $Diff<C, {| ...RSP; ...MDP |
  * @param actionCreators the action creator used to bind actions.
  * @return {*} the connect function that connects a react component.
  */
-export function fullConnect<C: Object, RSP: Object, MDP: Object>( // flowlint-line unclear-type:off
+export function fullConnect<-C, -RSP, -MDP>(
   mapStateToProps: (state: State, ownProps: OwnProp<C, RSP, MDP>) => RSP,
   actionCreators: MDP,
 ): (ComponentType<C>) => (ConnectedComponent<OwnProp<C, RSP, MDP>>) {
@@ -37,9 +35,9 @@ export function fullConnect<C: Object, RSP: Object, MDP: Object>( // flowlint-li
  * @param mapStateToProps the normal mapStateToProps function.
  * @return {*} the connect function that connects a react component.
  */
-export function stateConnect<Config: Object, RSP: Object>( // flowlint-line unclear-type:off
-  mapStateToProps: (state: State, ownProps: $Diff<Config, RSP>) => RSP,
-): (component: ComponentType<Config>) => ConnectedComponent<Config, RSP> {
+export function stateConnect<-C, -RSP>(
+  mapStateToProps: (state: State, ownProps: $Diff<C, RSP>) => RSP,
+): (ComponentType<C>) => ConnectedComponent<C, RSP> {
   return connect(mapStateToProps, null);
 }
 
@@ -49,8 +47,8 @@ export function stateConnect<Config: Object, RSP: Object>( // flowlint-line uncl
  * @param actionCreators the action creator used to bind actions.
  * @return {*} the connect function that connects a react component.
  */
-export function dispatchConnect<Config: Object, MDP: Object>( // flowlint-line unclear-type:off
+export function dispatchConnect<-C, -MDP>(
   actionCreators: MDP,
-): (component: ComponentType<Config>) => ConnectedComponent<Config, MDP> {
+): (ComponentType<C>) => ConnectedComponent<C, MDP> {
   return connect(null, actionCreators);
 }
