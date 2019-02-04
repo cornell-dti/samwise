96c4ac8b41e1cc87c6d913660eb03d6321c90bc0
Sam <sam@developersam.com>
2019-02-04 01:33:04 -0500

    Customize port for heroku

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
+PORT = os.environ.get('PORT', 5000)
 DEBUG = os.environ.get('DEBUG', '').lower() == 'true'
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
--- backend/run.py
+++ backend/run.py
@@ -13,4 +13,4 @@ def static_proxy(path):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=config.DEBUG)
+    app.run(host='0.0.0.0', port=config.PORT, debug=config.DEBUG)
