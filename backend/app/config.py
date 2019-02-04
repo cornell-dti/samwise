8443213f70d6cc24103a945dba2df280397ad344
Sam <sam@developersam.com>
2019-02-03 22:13:53 -0500

    Fix DEBUG env-var reading

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG', '').lower() == 'true'
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
