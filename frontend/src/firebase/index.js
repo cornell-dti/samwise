e50f6b047760a0b9b798ac82423c4ad972cd4948
Sam <sam@developersam.com>
2019-02-16 19:14:09 -0500

    Added firebase config

--- frontend/src/firebase/index.js
+++ frontend/src/firebase/index.js
@@ -3,6 +3,8 @@
 import firebase from 'firebase/app';
 
 if (process.env.NODE_ENV === 'production') {
+  // TODO enable it again later
+  /*
   firebase.initializeApp({
     apiKey: 'AIzaSyBrnR-ai3ZQrr3aYnezDZTZdw9e2TWTRtc',
     authDomain: 'dti-samwise.firebaseapp.com',
@@ -10,6 +12,15 @@ if (process.env.NODE_ENV === 'production') {
     projectId: 'dti-samwise',
     storageBucket: 'dti-samwise.appspot.com',
     messagingSenderId: '114434220691',
+  });
+  */
+  firebase.initializeApp({
+    apiKey: 'AIzaSyBJ0ElrYrw-fAQWWUEiRlf1wF38DswRduE',
+    authDomain: 'samwise-dev.firebaseapp.com',
+    databaseURL: 'https://samwise-dev.firebaseio.com',
+    projectId: 'samwise-dev',
+    storageBucket: 'samwise-dev.appspot.com',
+    messagingSenderId: '423488925083',
   });
 } else {
   firebase.initializeApp({
