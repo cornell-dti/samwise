8aadcc1a040c6acd28b93f67af4e21c944a1a341
Sam <sam@developersam.com>
2019-03-09 23:39:47 -0500

    Remove unused import

--- frontend/src/components/TaskView/FocusView/ClearFocus.tsx
+++ frontend/src/components/TaskView/FocusView/ClearFocus.tsx
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import { SubTask, Task, State } from '../../../store/store-types';
 import SquareTextButton from '../../UI/SquareTextButton';
 import { clearFocus } from '../../../firebase/actions';
-import styles from './FocusView.css';
 
 type Props = {
   readonly tasks: Map<string, Task>;
