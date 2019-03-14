9972d915bc17f387a3111a4f016b03a16af52521
Sam <sam@developersam.com>
2019-03-14 10:44:07 -0400

    added jest test for a simple util

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const merge = require('webpack-merge');
 const commonFunction = require('./webpack.common.js');
 
