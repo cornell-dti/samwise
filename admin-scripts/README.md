346036bc5f106bc57b3e8a65abd7027d9b87f5d2
Jessica Hong <jwh296@cornell.edu>
2019-03-16 16:05:52 -0400

    added remove old tasks script

---
+++ admin-scripts/README.md
@@ -0,0 +1,3 @@
+run python scripts with "python (insert file name).py"
+
+In order to run these scripts, you need to download a private key from the firebase console. Go to the firebase console for samwise-dev and navigate to Project Settings. Then navigate to service accounts and click generate new private key. Rename this file to "firebaseadmin.json" and place it in the admin-scripts folder. 
---
+++ admin-scripts/removeOldTasks.py
@@ -0,0 +1,19 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import datetime
+from firebase_admin import firestore
+
+cred = credentials.Certificate('firebaseadmin.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+tasks = db.collection(u'samwise-tasks')
+cutoff = datetime.datetime.today() - datetime.timedelta(days=(60))
+oldTasks = tasks.where(u'date', u'<', cutoff).get()
+
+for task in oldTasks:
+    for subtask in task.get(u'children'):
+        subtask.delete()
+    print(u'Document data: {}'.format(task.to_dict()))
+    task.reference.delete()
