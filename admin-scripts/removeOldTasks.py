d2d0a951f77a70a34e5fed26e6b24e4a02ffde15
Jessica Hong <jwh296@cornell.edu>
2019-03-19 15:19:16 -0400

    fixed removeOldTasks subtask deletion to handle string children

--- admin-scripts/removeOldTasks.py
+++ admin-scripts/removeOldTasks.py
@@ -15,6 +15,7 @@ oldTasks = tasks.where(u'date', u'<', cutoff).get()
 for task in oldTasks:
     for subtask in task.get(u'children'):
         #TODO: Fix delete children by name not reference
+        subtask = db.collection(u'samwise-subtasks').document(subtask).get()
         subtask.delete()
     print(u'Document data: {}'.format(task.to_dict()))
     task.reference.delete()
