b4c81090f2a948c733d650ca5dd585e06b80fa8a
meganyin13 <my474@cornell.edu>
2019-04-10 20:21:33 -0400

    fixed syntax

--- frontend/src/components/UI/Tooltip.tsx
+++ frontend/src/components/UI/Tooltip.tsx
@@ -1,50 +1,43 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import SamwiseIcon from './SamwiseIcon';
 import { IconName } from './Samwise-icon-types';
 
-type props = {
+type Props = {
   readonly name: string;
-  readonly iconName: string;
+  readonly iconName: IconName;
 }
-type states = {
-  name: string;
-  iconName: IconName;
+type State = {
   hover: boolean;
 }
 
-class Tooltip extends React.Component<props, states> {
-  public constructor(props: any) {
-    super(props);
-    this.state = {
-      name: props.name,
-      iconName: props.iconName,
-      hover: false,
-    };
-  }
+class Tooltip extends React.Component<Props, State> {
+  public readonly state: State = { hover: false };
 
-  public handleMouseIn() {
+  private handleMouseIn = () => {
     this.setState({ hover: true });
-  }
+  };
 
-  public handleMouseOut() {
+  private handleMouseOut = () => {
     this.setState({ hover: false });
-  }
+  };
 
-  public render() {
-    const { name, iconName, hover } = this.state;
+  public render(): ReactElement {
+    const { name, iconName } = this.props;
+    const { hover } = this.state;
     const tooltipStyle = {
       display: hover ? 'block' : 'none',
     };
 
     return (
       <div>
-        <SamwiseIcon
-          iconName={iconName}
-          onMouseOver={this.handleMouseIn.bind(this)}
-          onMouseOut={this.handleMouseOut.bind(this)}
+        <div
+          onMouseOver={this.handleMouseIn}
+          onMouseOut={this.handleMouseOut}
         >
-          {name}
-        </SamwiseIcon>
+          <SamwiseIcon iconName={iconName}>
+            {name}
+          </SamwiseIcon>
+        </div>
         <div>
           <div style={tooltipStyle}>this is the tooltip!!</div>
         </div>
