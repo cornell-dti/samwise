b650989038d4b90c1d692de96eb66ee313968b24
Jessica Hong <jwh296@cornell.edu>
2019-04-28 16:25:09 -0400

    daily sum

--- admin-scripts/sumActions.py
+++ admin-scripts/sumActions.py
@@ -4,14 +4,7 @@ from firebase_admin import db
 import datetime
 from firebase_admin import firestore
 from collections import Counter
-
-def get_records(docs, start_date, end_date):
-    for temp, entry in docs.items():
-        print(start_date)
-        print(end_date)
-        print(entry['time'])
-        if start_date <= entry['time'] < end_date:
-            print(entry)
+import pytz
 
 cred = credentials.Certificate('firebaseadmin.json')
 firebase_admin.initialize_app(cred)
@@ -19,11 +12,7 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 actions = db.collection(u'samwise-user-actions')
 
-min_time = actions.order_by('time').limit(1).get()
-for ob in min_time:
-    min_time = ob.get('time')
-
-min_day = datetime.datetime(2019, 3, 3)
+min_day = datetime.datetime(2019, 3, 3, )
 today = datetime.datetime.now()
 num_days = ((today-min_day).days)
 
@@ -31,48 +20,34 @@ usernames = set()
 for ob in actions.get():
     usernames.add(ob.get('user'))
 
-print(usernames)
+all_actions = ['editTask', 'editTag', 'deleteTag', 'createSubTask', 
+'completeFocusedTask', 'deleteTask', 'createTag', 'focusTask', 'completeTask',
+ 'createTask', 'deleteSubTask']
+
+#create daily sum for actions
 for user in usernames:
-    start = min_day + datetime.timedelta(days=(0))
-    end = start + datetime.timedelta(days=(1))
-    user_elems = actions.where('user', '==', user).get()
-    my_dict = { el.id: el.to_dict() for el in user_elems }
-    get_records(my_dict, start, end)
-    print(my_dict)
-    # for i in range(num_days):
-    #     start = min_day + datetime.timedelta(days=(i))
-    #     end = start + datetime.timedelta(days=(1))
-        
+    for i in range(num_days):
+        start = min_day + datetime.timedelta(days=(i))
+        end = start + datetime.timedelta(days=(1))
+        start = start.replace(tzinfo=pytz.UTC)
+        end = end.replace(tzinfo=pytz.UTC)
+        user_elems = actions.where('user', '==', user).get()
+        my_dict = { el.id: el.to_dict() for el in user_elems }
+        result = Counter()
+        for temp, entry in my_dict.items():
+            if start <= entry['time'] < end:
+                for key, value in entry['actions'].items():
+                    result[key] += value
 
-    #     print(day)
-    #     result = Counter()
-    #     for ob in day:
-    #         elem = ob.get('actions')
-    #         for key, value in elem.items():
-    #             result[key] += value
-
-    #     sum_day = {
-    #         'actions': dict(result),
-    #         'time': start,
-    #         'user': user,
-    #         'type': 'DAILY' 
-    #     }
-
-    #     print(sum_day)
-
-
-#daily sum
-# user = 'my474@cornell.edu'
-# day = actions.where('user', '=', user).where('time', '>', min_time).where('time', '<=', min_time + datetime.timedelta(days=(1))).get()
-# result = Counter()
-# for ob in day:
-#     elem = ob.get('actions')
-#     for key, value in elem.items():
-#         result[key] += value
-
-# actions.add
-
-
-#weekly sum
-
-#monthly sum
+        if (len(result) == 0):
+            continue
+        print("not empty")
+        for action in all_actions:
+            result[action] += 0
+        sum_day = {
+            'actions': dict(result),
+            'time': start,
+            'user': user,
+            'type': 'DAILY' 
+        }
+        db.collection('fake-user-actions').add(sum_day)
