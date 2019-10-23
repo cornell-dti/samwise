1f81dcb679e2d4da3e68702ba44f9ed9692fc805
Sam <sam@developersam.com>
2019-10-23 13:49:36 -0400

    Fix linter error

--- frontend/src/components/Util/SearchBox/DropdownItem.test.tsx
+++ frontend/src/components/Util/SearchBox/DropdownItem.test.tsx
@@ -4,7 +4,7 @@ import DropdownItem from './DropdownItem';
 
 it('DropdownItem matches snapshot.', () => {
   const tree = renderer.create(
-    <DropdownItem item={{ key: 42, value: 'foo-bar' }} className="test" onSelect={() => { }} />
+    <DropdownItem item={{ key: 42, value: 'foo-bar' }} className="test" onSelect={() => { }} />,
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
