674055e47de1eef3b50da1f20b671d15744e7d8f
meganyin13 <meganyin13@users.noreply.github.com>
2019-10-27 20:55:11 +0000

    Automated deployment: Sun Oct 27 20:55:11 UTC 2019 1dad08da0f56da002c3ca7587caad38b4cde1986

--- 338/service-worker.js
+++ 338/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/338/precache-manifest.a8a31fd6b1636ca36de34a1eb6704c51.js"
+  "/338/precache-manifest.0206608f8e071a05801124fe2c258c63.js"
 );
 
 self.addEventListener('message', (event) => {
