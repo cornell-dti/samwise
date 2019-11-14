ae85ccc68d32cd1af91a4d1bdf36a00708e68dd4
Jason Tung <jt568@cornell.edu>
2019-11-13 21:26:57 -0500

    can actually grab assignments and not courses now

--- functions/canvasFunctions.ts
+++ functions/canvasFunctions.ts
@@ -1,4 +1,5 @@
-import fetch, { Response } from 'node-fetch';
+import fetch, {Response} from 'node-fetch';
+import {parse, stringify} from 'json-bigint';
 
 // export const scheduledFunctionCrontab = (): void => functions.pubsub.schedule('0 0 * * *')
 //   .timeZone('America/New_York')
@@ -6,16 +7,36 @@ import fetch, { Response } from 'node-fetch';
 //     // stuff
 //   });
 
-async function canvasGet(path: string, token: string): Promise<any>{
-    const uri = `https://canvas.instructure.com/api/v1/${path}?access_token=${token}`;
-    await fetch(uri, {
-        method: 'GET'
-    }).then((response: Response) => response.json())
-        .then(json => {
-            console.log(json);
-        }).catch((err) => {
-        console.log(err);
-    });
+function getUri(path: string, token: string) {
+    return `https://canvas.instructure.com/api/v1/${path}?access_token=${token}`;
 }
 
-canvasGet('courses', '9713~v6sFYkNFHbgXE3Bgo1JBLP7LO18t2aFCs6cvgbcLAOGPA6ejv51ozK8fV92Jq3Hs');
+async function canvasGetAssignments(token: string): Promise<any> {
+    await fetch(getUri("courses", token), {
+        method: 'GET'
+    }).then((response: Response) => response.text())
+        .then(json => {
+            getAssignments(token, parse(json));
+        }).catch((err) => {
+            console.log(err);
+        });
+}
+
+async function getAssignments(token: string, data: JSON[]) {
+    let assignmentList = [];
+    await data.forEach(course => {
+        fetch(getUri(`courses/${course["id"].toString()}/assignments`, token), {
+            method: 'GET'
+        }).then((response: Response) => response.text())
+            .then(json => {
+                console.log(getUri(`courses/${course["id"]}/assignments`, token));
+                assignmentList.push(parse(json));
+                console.log(parse(json));
+            }).catch((err) => {
+            console.log(err);
+        });
+    });
+    console.log(assignmentList);
+};
+
+canvasGetAssignments('');
