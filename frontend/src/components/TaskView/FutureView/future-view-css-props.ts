c2f6c73f0cde93729d6713f40b7eb25555888a12
Sam <sam@developersam.com>
2019-11-17 17:41:46 -0500

    Do not expand floating view width to 300px

--- frontend/src/components/TaskView/FutureView/future-view-css-props.ts
+++ frontend/src/components/TaskView/FutureView/future-view-css-props.ts
@@ -7,8 +7,3 @@ export const headerHeight = 66;
  * Height of each task line in display.
  */
 export const taskHeight = 25;
-
-/**
- * The width of the floating view.
- */
-export const floatingViewWidth = 300;
