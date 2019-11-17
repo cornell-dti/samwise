d082acf22053f514473b99ac70c4a3f33f2f0705
Sam <sam@developersam.com>
2019-11-17 16:41:08 -0500

    Cleanup RepeatFrequencyHeader

--- frontend/src/components/Util/TaskEditors/TaskEditor/RepeatFrequencyHeader.tsx
+++ frontend/src/components/Util/TaskEditors/TaskEditor/RepeatFrequencyHeader.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react';
 import SamwiseIcon from '../../../UI/SamwiseIcon';
 import { Tag, RepeatMetaData } from '../../../../store/store-types';
 import { DAYS_IN_WEEK, isBitSet } from '../../../../util/bitwise-util';
+import styles from './RepeatFrequencyHeader.module.css';
 
 type Props = {
   readonly date: Date | RepeatMetaData;
@@ -12,7 +13,31 @@ type Props = {
 type BinaryCount = {
   binary: number;
   count: number;
-}
+};
+
+const arrOfWeeks = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const patternTypeToString = (patternType: string): string => {
+  switch (patternType) {
+    case 'WEEKLY': return 'week';
+    case 'BIWEEKLY': return 'two weeks';
+    case 'MONTHLY': return 'month';
+    default:
+      throw new Error();
+  }
+};
+
+const bitsetToBinaryCount = (bit: number): BinaryCount => {
+  let binary = 0;
+  let count = 0;
+  for (let i = 0; i < DAYS_IN_WEEK; i += 1) {
+    if (isBitSet(bit, i, DAYS_IN_WEEK)) {
+      binary += (1 * 10 ** (DAYS_IN_WEEK - 1 - i));
+      count += 1;
+    }
+  }
+  return { binary, count };
+};
 
 export default function RepeatFrequencyHeader(
   { date, tag, getTag }: Props,
@@ -20,47 +45,6 @@ export default function RepeatFrequencyHeader(
   if (date instanceof Date) {
     return null;
   }
-
-  const frequencyHeaderStyle = {
-    color: getTag(tag).color,
-    background: 'rgba(255, 255, 255, 0.6)',
-    padding: '3px',
-    height: '100%',
-    fontSize: '10px',
-  };
-
-  const repeatIconStyle = {
-    fill: '#FFFFFF',
-    marginRight: '4px',
-    marginBottom: '-5px',
-    height: '16px',
-    width: '16px',
-  };
-
-  const patternTypeToString = (patternType: string): string => {
-    switch (patternType) {
-      case 'WEEKLY': return 'week';
-      case 'BIWEEKLY': return 'two weeks';
-      case 'MONTHLY': return 'month';
-      default:
-        throw new Error();
-    }
-  };
-
-  const bitsetToBinaryCount = (bit: number): BinaryCount => {
-    let binary = 0;
-    let count = 0;
-    for (let i = 0; i < DAYS_IN_WEEK; i += 1) {
-      if (isBitSet(bit, i, DAYS_IN_WEEK)) {
-        binary += (1 * 10 ** (DAYS_IN_WEEK - 1 - i));
-        count += 1;
-      }
-    }
-    return { binary, count };
-  };
-
-  const arrOfWeeks = ['Sunday', 'Monday', 'Tuesday',
-    'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
   const getRepeatedDays = (bit: number): string => {
     const frequency = bitsetToBinaryCount(bit);
@@ -83,11 +67,8 @@ export default function RepeatFrequencyHeader(
   };
 
   return (
-    <div style={frequencyHeaderStyle}>
-      <SamwiseIcon
-        iconName="repeat-frequency"
-        style={repeatIconStyle}
-      />
+    <div className={styles.Header} style={{ color: getTag(tag).color }}>
+      <SamwiseIcon iconName="repeat-frequency" className={styles.Icon} />
       {getRepeatedDays(date.pattern.bitSet)}
     </div>
   );
