c1d98ef431610e08c41512d248fd17429d81b9a4
Jason Tung <jt568@cornell.edu>
2019-11-17 10:43:19 -0500

    starting work on inserting into firebase

--- functions/iCalFunctions.ts
+++ functions/iCalFunctions.ts
@@ -1,20 +1,46 @@
 import {fromURL} from 'ical'
+import {getNewTaskId} from '../frontend/src/firebase/id-provider'
+import getOrder from '../frontend/src/firebase/order-manager'
+import {CommonTask, OneTimeTask} from '../frontend/src/store/store-types'
+import {TaskWithoutIdOrderChildren} from "../frontend/src/firebase/actions";
 
-let calLink: string = '';
-function parseICal(link:string):void{
-    fromURL(link, {}, function (err, data) {
+//todo make this work with repeating tasks & support tags
+let calLink: string = 'http://p49-caldav.icloud.com/published/2/MjU1MjYxMDc0MjQyNTUyNrbuSqmZmLMJBaHe_zS6XS6_mwNxYp-dEIrIBLSXyiw7';
+
+function parseICal(link: string): void {
+    fromURL(link, {}, async function (err, data) {
         // console.log(err);
         // console.log(data);
         for (let k in data) {
             if (data.hasOwnProperty(k)) {
                 var ev = data[k];
                 if (data[k].type == 'VEVENT') {
-                    console.log(ev['summary']);
-                    console.log(new Date(ev['end']));
+                    const taskName = ev['summary'];
+                    const endDate = new Date(ev['end']);
+                    const taskID: string = getNewTaskId();
+                    const order: number = await getOrder('tasks');
+                    const tag: string = "THE_GLORIOUS_NONE_TAG";
+                    const children: Set<string> = new Set<string>();
+                    const newTask: CommonTask<Date> = {
+                        id: taskID,
+                        order: order,
+                        name: taskName,
+                        tag: tag,
+                        date: endDate,
+                        complete: false,
+                        inFocus: false,
+                        children: children
+                    };
+                    const newTaskOneTime:OneTimeTask = newTask & {};
+                    const taskWrapper:TaskWithoutIdOrderChildren =
                 }
             }
         }
-    });
+    }
+}
+
+)
+;
 }
 
 parseICal(calLink);
