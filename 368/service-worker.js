25d11ec0e4bc5b1cd89be55863ce48a61e31bc1c
SamChou19815 <SamChou19815@users.noreply.github.com>
2019-11-25 18:03:17 +0000

    Automated deployment: Mon Nov 25 18:03:17 UTC 2019 af462ede30098d4175b952d424639356b04fdcc3

--- 368/service-worker.js
+++ 368/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/368/precache-manifest.f7d44dd245e41fc754f3bf336ba3d59c.js"
+  "/368/precache-manifest.8f262a84f51f49285a786fcde735536c.js"
 );
 
 self.addEventListener('message', (event) => {
