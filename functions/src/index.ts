29f720db4784e8b7d27e43ce22ef1255c60386ce
Sam <sam@developersam.com>
2019-12-06 14:41:47 -0500

    Make functions in a deployable form
    This commit only makes the code in-theory deployable. We still need to do some magic to overcome firebase's lack of support of yarn workspace

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,11 @@
 import * as functions from 'firebase-functions';
-import getICalLink from "./iCalFunctions";
+import getICalLink from './iCalFunctions';
 
-export default function autoGrabICal() {
-    functions.pubsub.schedule('0 0 * * *').onRun((context:functions.EventContext) => {
-        getICalLink().catch(err => console.log(err)).then(r => {
-            console.log("finished getICalLink()")
-        }) .catch(err => console.log(err));
-    });
-}
+functions.pubsub.schedule('0 0 * * *').onRun(() => {
+  getICalLink()
+    .catch((err) => console.log(err))
+    .then(() => {
+      console.log('finished getICalLink()');
+    })
+    .catch((err) => console.log(err));
+});
