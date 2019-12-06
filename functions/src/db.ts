521f6dbe22d479b10c2f3b499e372b71213f87d6
Sam <sam@developersam.com>
2019-12-06 14:27:06 -0500

    Purge unused functions

--- functions/src/db.ts
+++ functions/src/db.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin';
 import serviceAccount from './firebase-adminsdk.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   databaseURL: 'https://samwise-dev.firebaseio.com',
 });
 
@@ -13,26 +13,17 @@ admin.initializeApp({
  */
 export const db = (): admin.firestore.Firestore => admin.firestore();
 
-
 /**
  * Collection name literals.
  */
 const collections = {
   ORDER_MANAGER: 'samwise-order-manager',
   USER_SETTINGS: 'samwise-settings',
-  BANNER_MESSAGE: 'samwise-banner-message',
-  TAGS: 'samwise-tags',
   TASKS: 'samwise-tasks',
-  SUBTASKS: 'samwise-subtasks',
 };
 
 type Collection = admin.firestore.CollectionReference;
 
 export const orderManagerCollection = (): Collection => db().collection(collections.ORDER_MANAGER);
 export const settingsCollection = (): Collection => db().collection(collections.USER_SETTINGS);
-export const bannerMessageStatusCollection = (): Collection => db().collection(
-  collections.BANNER_MESSAGE,
-);
-export const tagsCollection = (): Collection => db().collection(collections.TAGS);
 export const tasksCollection = (): Collection => db().collection(collections.TASKS);
-export const subTasksCollection = (): Collection => db().collection(collections.SUBTASKS);
--- functions/src/iCalFunctions.ts
+++ functions/src/iCalFunctions.ts
@@ -3,15 +3,18 @@ import { fromURL } from 'ical';
 import { settingsCollection, tasksCollection } from './db';
 import getOrder from './order-manager';
 
-export default async function getICalLink() {
-    await settingsCollection().where("canvasCalendar", ">", "").get().then(function (querySnapshot) {
-        querySnapshot.forEach(function (doc) {
-            try {
-                parseICal(doc.data()['canvasCalendar'], doc.id);
-            } catch {
-                console.log("Failed to use this calendar link");
-            }
-        });
+export default async function getICalLink(): Promise<void> {
+  await settingsCollection()
+    .where('canvasCalendar', '>', '')
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        try {
+          parseICal(doc.data().canvasCalendar, doc.id);
+        } catch {
+          console.log('Failed to use this calendar link');
+        }
+      });
     })
     .catch((error) => {
       console.log('Error getting documents: ', error);
@@ -19,44 +22,51 @@ export default async function getICalLink() {
 }
 
 export function parseICal(link: string, user: string): void {
-    fromURL(link, {}, async function (err, data) {
-        const today = new Date();
-        for (const k in data) {
-            if (data.hasOwnProperty(k)) {
-                const ev = data[k];
-                if (data[k].type === 'VEVENT') {
-                    const taskName = ev['summary'];
-                    //the unique id i will use is a concat of the user and event uid because uids are not unique between
-                    // users
-                    const uid = ev['uid'] + user;
-                    const endObject: any = ev['end'];
-                    const endDate = endObject === null ? null : new Date(endObject.getTime());
-                    const taskID: string = tasksCollection().doc().id;
-                    const order: number = await getOrder(user, 'tasks');
-                    if (endDate === null) {
-                        continue;
-                    }
-                    if (endDate > today) {
-                        await tasksCollection().where("icalUID", "==", uid).get().then(async function (querySnapshot) {
-                            if (querySnapshot.size === 0) {
-                                await tasksCollection().doc(taskID).set({
-                                    children: [],
-                                    complete: false,
-                                    date: endDate,
-                                    inFocus: false,
-                                    name: taskName,
-                                    order: order,
-                                    owner: user,
-                                    tag: 'THE_GLORIOUS_NONE_TAG',
-                                    type: 'ONE_TIME',
-                                    icalUID: uid
-                                }).catch((e: Error) => console.log(e));
-                            }
-                        });
-                    }
+  fromURL(link, {}, async (_, data) => {
+    const today = new Date();
+    for (const k in data) {
+      // eslint-disable-next-line no-prototype-builtins
+      if (data.hasOwnProperty(k)) {
+        const ev = data[k];
+        if (data[k].type === 'VEVENT') {
+          const taskName = ev.summary;
+          // the unique id i will use is a concat of the user and event uid,
+          // because uids are not unique between users.
+          const uid = ev.uid + user;
+          const endObject: any = ev.end;
+          const endDate = endObject === null ? null : new Date(endObject.getTime());
+          const taskID: string = tasksCollection().doc().id;
+          const order: number = await getOrder(user, 'tasks');
+          if (endDate === null) {
+            // eslint-disable-next-line no-continue
+            continue;
+          }
+          if (endDate > today) {
+            await tasksCollection()
+              .where('icalUID', '==', uid)
+              .get()
+              .then(async (querySnapshot) => {
+                if (querySnapshot.size === 0) {
+                  await tasksCollection()
+                    .doc(taskID)
+                    .set({
+                      children: [],
+                      complete: false,
+                      date: endDate,
+                      inFocus: false,
+                      name: taskName,
+                      order,
+                      owner: user,
+                      tag: 'THE_GLORIOUS_NONE_TAG',
+                      type: 'ONE_TIME',
+                      icalUID: uid,
+                    })
+                    .catch((e: Error) => console.log(e));
                 }
               });
           }
         }
-    });
+      }
+    }
+  });
 }
