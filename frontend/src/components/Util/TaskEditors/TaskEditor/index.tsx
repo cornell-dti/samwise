cornell-dti/samwise

commit 28d48285277da1113336fda68843a4c7a13e34dc

Call `onSave` when `TaskEditor` lose focus (#373)
### Summary

Suppose you exit before the TaskEditor can save, then the edit is forever lost.
This diff ensures that things are always saved by calling `onSave` before `onMouseLeave` and `onBlur`, two events that indicates that TaskEditor might exit.

With this change, we can increase the interval of running `onSave` to a higher value: 1000ms.

### Test Plan

Play with `TaskEditor`. Type something really fast and then let mouse leave the editor. Things should be saved.

frontend/src/components/Util/TaskEditors/TaskEditor/index.tsx
@@ -99,6 +99,7 @@ function TaskEditor(
   const [subTaskToFocus, setSubTaskToFocus] = useState<TaskToFocus>(null);
 
   const onMouseLeave = (): void => {
+    onSave();
     if (onBlur) {
       onBlur();
     }
@@ -204,7 +205,7 @@ function TaskEditor(
       if (type === 'ONE_TIME') {
         onSave();
       }
-    }, 500);
+    }, 1000);
     return () => clearInterval(intervalID);
   }, [type, onSave]);
 
@@ -215,7 +216,7 @@ function TaskEditor(
       onMouseEnter={onFocus}
       onMouseLeave={onMouseLeave}
       onFocus={onFocus}
-      onBlur={ignore}
+      onBlur={onMouseLeave}
       ref={editorRef}
     >
       {isOverdue && <OverdueAlert target="task-card" />}

commit 34e52b72d37dd10408a7124e2d961720e932a508

[editor] Guard implicit `onSave` call with `type === 'ONE_TIME'` check
### Summary

This is a regression introduced in #373.
We shouldn't unconditionally call onSave because it will trigger a modal during repeating task edit. The current master will popup the modal whenever you type any character. (In other words, current master's repeating task is broken.)

### Test Plan

Observe the bug in current master. The bug is gone in this branch.

frontend/src/components/Util/TaskEditors/TaskEditor/index.tsx
@@ -99,7 +99,9 @@ function TaskEditor(
   const [subTaskToFocus, setSubTaskToFocus] = useState<TaskToFocus>(null);
 
   const onMouseLeave = (): void => {
-    onSave();
+    if (type === 'ONE_TIME') {
+      onSave();
+    }
     if (onBlur) {
       onBlur();
     }