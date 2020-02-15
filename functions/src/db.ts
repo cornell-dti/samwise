57c3f84b8dec1f4868c65b531de1071d530a07e2
Sam Zhou <sam@developersam.com>
2020-02-15 11:49:56 -0500

    Automatically remove old non-repeating tasks (#418)
    * Automatically remove old non-repeating tasks
    
    ### Summary
    
    This diff adds a function that removes all old non-repeating tasks.
    The function currently uses 40 as the upper limit to try to be conservative, because if we allow user to see tasks a month ago, it can still be supported.
    
    While testing the function, I verified that Michael's report that the deploy script is indeed broken, because I was using an old globally installed firebase CLI before. It's fixed in this diff. To prevent similar problems in the future, I integrated firebase functions deployment into continuous deployment script.
    
    ### Test Plan
    
    Deployed to staging to see it's effect. Looks like it works.
    
    I also write a unit test for the `partition` helper function, and the test is run by `react-scripts`. Unfortunately `react-scripts` automatically overrides some json fields in tsconfig.json, so I have to override it back in tsc CLI in package.json. The alternative would be setting up jest manually, but it requires an additional jest and an additional babel config, and I hate writing those configs.
    
    * Create dummy credential in CI test environment
    
    * Remove python script that does the same thing
    
    The python script can be run only manually, while the function is automatically run every day.
    
    * Skip initialize firebase-admin in test mode

--- functions/src/db.ts
+++ functions/src/db.ts
@@ -2,10 +2,15 @@ import * as admin from 'firebase-admin';
 import Database from 'common/lib/firebase/database';
 import serviceAccount from './firebase-adminsdk.json';
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  databaseURL: 'https://samwise-dev.firebaseio.com',
-});
+if (process.env.TEST_MODE) {
+  // eslint-disable-next-line no-console
+  console.log('We are in test mode. Skip initializing firebase-admin.');
+} else {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    databaseURL: 'https://samwise-dev.firebaseio.com',
+  });
+}
 
 const database = new Database(() => admin.firestore());
 export default database;
