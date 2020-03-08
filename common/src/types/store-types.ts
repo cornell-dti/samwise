71d2a1281a69531c6da9286dd0101631f790bfa3
meganyin13 <my474@cornell.edu>
2020-03-08 04:18:28 -0400

    add futureViewOrder to CommonTask type

--- common/src/types/store-types.ts
+++ common/src/types/store-types.ts
@@ -26,6 +26,7 @@ export type PartialSubTask = Partial<SubTaskWithoutIdOrder>;
 export type CommonTask<D> = {
   readonly id: string;
   readonly order: number;
+  readonly futureViewOrder?: number;
   readonly name: string; // Example: "Task 1 name"
   readonly tag: string; // ID of the tag
   readonly date: D;
@@ -39,7 +40,6 @@ type FlexibleCommonTask = CommonTask<Date | RepeatMetaData>;
 export type OneTimeTask = CommonTask<Date> & {
   readonly type: 'ONE_TIME';
   readonly icalUID?: string;
-  readonly futureViewOrder?: number;
 };
 
 /**
