24b1be63dd37c84f6cadbf942c092b6476ffbe4f
meganyin13 <my474@cornell.edu>
2020-03-08 13:24:25 -0400

    remove references to assign-future-view-order.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,6 @@ import * as functions from 'firebase-functions';
 import getICalLink from './iCalFunctions';
 import focusTasksDueToday from './focus-today-task';
 import removeOldTasks from './remove-old-tasks';
-import assignFutureViewOrder from './assign-future-view-order';
 
 export const iCalFunction = functions.pubsub.schedule('0 0 * * *').onRun(() => {
   getICalLink()
@@ -16,5 +15,3 @@ export const iCalFunction = functions.pubsub.schedule('0 0 * * *').onRun(() => {
 export const FocusTasksDueToday = functions.pubsub.schedule('0 0 * * *').onRun(focusTasksDueToday);
 
 export const RemoveOldTasks = functions.pubsub.schedule('0 0 * * *').onRun(removeOldTasks);
-
-export const AssignFutureViewOrder = functions.pubsub.schedule('every 1 minute').onRun(assignFutureViewOrder);
