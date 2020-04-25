28265c512636eef6c95d838441d91d83a6011b66
Pinxuan Huang <pinxhuang@gmail.com>
2020-04-25 12:28:41 -0400

    Make buttons clickable

--- frontend/src/components/GroupView/RightView/GroupTaskRow/index.tsx
+++ frontend/src/components/GroupView/RightView/GroupTaskRow/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, SyntheticEvent, KeyboardEvent } from 'react';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import GroupTasksContainer from './GroupTasksContainer';
@@ -8,6 +8,17 @@ type Props = {
   memberName: string;
 }
 
+const clickAddGroupTask = (e: SyntheticEvent<HTMLElement>): void => {
+  e.stopPropagation();
+  console.log('clicked add group task');
+};
+const pressedAddGroupTask = (e: KeyboardEvent): void => {
+  if (e.key === 'Enter' || e.key === ' ') {
+    e.stopPropagation();
+    console.log('pressed add group task');
+  }
+};
+
 export default ({ memberName }: Props): ReactElement => {
   const initials = `${memberName.split(' ')[0].charAt(0)}${memberName.split(' ')[1].charAt(0)}`;
 
@@ -15,7 +26,12 @@ export default ({ memberName }: Props): ReactElement => {
     <div className={styles.GroupTaskRow}>
       <div className={styles.Initials}>{initials}</div>
 
-      <div className={styles.AddTaskContainer}>
+      <div
+        role="presentation"
+        className={styles.AddTaskContainer}
+        onClick={clickAddGroupTask}
+        onKeyPress={pressedAddGroupTask}
+      >
         <div className={styles.AddTask}>
           <FontAwesomeIcon icon={faPlus} />
           <h3>Assign new task</h3>
--- frontend/src/components/GroupView/RightView/index.tsx
+++ frontend/src/components/GroupView/RightView/index.tsx
@@ -8,14 +8,23 @@ type Props = {
   groupMemberNames: string[];
 }
 
+const EditGroupNameIcon = (): ReactElement => {
+  const handler = (): void => {
+    console.log('edit group');
+  };
+  return <SamwiseIcon iconName="pencil" className={styles.EditGroupNameIcon} onClick={handler} />;
+};
+
 export default ({ groupName, groupMemberNames }: Props): ReactElement => (
   <div className={styles.RightView}>
     <div>
       <h2>{groupName}</h2>
-      <SamwiseIcon iconName="pencil" />
+      <EditGroupNameIcon />
     </div>
-    {
-      groupMemberNames.map((m) => <GroupTaskRow memberName={m} key={m} />)
-    }
+    <div className={styles.GroupTaskRowContainer}>
+      {
+        groupMemberNames.map((m) => <GroupTaskRow memberName={m} key={m} />)
+      }
+    </div>
   </div>
 );
