32500d4c5a7efb4104a508f5682c88ce011a260e
mt-xing <mt-xing@users.noreply.github.com>
2020-05-08 23:41:40 -0400

    View and Reject Invitations

---
+++ frontend/src/components/TitleBar/GroupInvites/index.tsx
@@ -0,0 +1,43 @@
+import React, { ReactElement } from 'react';
+import { connect } from 'react-redux';
+import { Map } from 'immutable';
+import { State, PendingGroupInvite } from 'common/lib/types/store-types';
+import styles from './index.module.css';
+import { rejectInvite } from '../../../firebase/actions';
+
+type Props = {
+  readonly pendingInvites: Map<string, PendingGroupInvite>;
+};
+
+function SingleInvitation(
+  inviter: string, groupID: string,
+): ReactElement {
+  return (
+    <li key={groupID}>
+      <span className={styles.Text}>{inviter} has invited you to join their group project.</span>
+      <div className={styles.ButtonWrap}>
+        <button type="button" onClick={() => { console.log('Tried to join group, a currently unsupported operation'); }}>
+            Join
+        </button>
+        <button type="button" onClick={() => { rejectInvite(groupID); }}>
+            Reject
+        </button>
+      </div>
+    </li>
+  );
+}
+
+const GroupInvites = ({ pendingInvites }: Props): ReactElement | null => {
+  if (pendingInvites.isEmpty()) { return null; }
+  return (
+    <ul className={styles.Banner}>
+      {pendingInvites.valueSeq().map((val) => SingleInvitation(
+        val.inviterName,
+        val.group,
+      ))}
+    </ul>
+  );
+};
+
+const Connected = connect(({ pendingInvites }: State) => ({ pendingInvites }))(GroupInvites);
+export default Connected;
