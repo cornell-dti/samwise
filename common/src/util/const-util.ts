72e7efc8fc1aea13b43d89b6637376b30b52d0fc
Sam <sam@developersam.com>
2020-08-22 22:42:15 -0400

    Update semester end dates

--- common/src/util/const-util.ts
+++ common/src/util/const-util.ts
@@ -1,10 +1,10 @@
 
 /**
- * The (exclusive) last day of class for Spring 2020
+ * The (exclusive) last day of class for Fall 2020
  */
-export const LAST_DAY_OF_CLASS = new Date('5/13/2020');
+export const LAST_DAY_OF_CLASS = new Date('12/16/2020');
 
 /**
- * The (exclusive) last day of final exams for Spring 2020
+ * The (exclusive) last day of final exams for Fall 2020
  */
-export const LAST_DAY_OF_EXAMS = new Date('5/24/2020');
+export const LAST_DAY_OF_EXAMS = new Date('12/21/2020');
