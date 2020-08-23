a043740fec3a5d4a5e683fc6766d169fba2d9240
Sam Zhou <sam@developersam.com>
2020-08-23 00:23:02 -0400

    Make end dates exclusive

--- common/src/util/const-util.ts
+++ common/src/util/const-util.ts
@@ -2,9 +2,9 @@
 /**
  * The (exclusive) last day of class for Fall 2020
  */
-export const LAST_DAY_OF_CLASS = new Date('12/16/2020');
+export const LAST_DAY_OF_CLASS = new Date('12/17/2020');
 
 /**
  * The (exclusive) last day of final exams for Fall 2020
  */
-export const LAST_DAY_OF_EXAMS = new Date('12/21/2020');
+export const LAST_DAY_OF_EXAMS = new Date('12/22/2020');
