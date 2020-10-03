7d420f236570b09fa64bcb2a07a5c02aed013fbb
Pinxuan Huang <pinxhuang@gmail.com>
2020-10-03 10:25:14 -0400

    Add SearchGroupMember to GroupMemberPicker

--- frontend/src/components/TaskCreator/GroupMemberPicker.tsx
+++ frontend/src/components/TaskCreator/GroupMemberPicker.tsx
@@ -1,10 +1,10 @@
-import React, { ReactElement, KeyboardEvent } from 'react';
-import { connect } from 'react-redux';
-import { NONE_TAG, NONE_TAG_ID } from 'common/util/tag-util';
-import { State, Tag } from 'common/types/store-types';
-import TagListPicker from '../Util/TagListPicker/TagListPicker';
-import styles from './Picker.module.scss';
-import SamwiseIcon from '../UI/SamwiseIcon';
+import React, { ReactElement, KeyboardEvent } from "react";
+import { connect } from "react-redux";
+import { NONE_TAG, NONE_TAG_ID } from "common/util/tag-util";
+import { State, Tag, GroupMember } from "common/types/store-types";
+import SearchGroupMember from "../Util/GroupMemberListPicker/SearchGroupMember";
+import styles from "./Picker.module.scss";
+import SamwiseIcon from "../UI/SamwiseIcon";
 
 type OwnProps = {
   readonly tag: string;
@@ -16,6 +16,14 @@ type Props = OwnProps & {
   // subscribed from redux store.
   readonly getTag: (id: string) => Tag;
 };
+
+// hardcoded member list
+const members: GroupMember[] = [
+  { netId: "dl123", name: "Darien Lopez" },
+  { netId: "sj234", name: "Sarah Johnson" },
+  { netId: "mp678", name: "Michelle Parker" },
+  { netId: "sj99", name: "Sarah Jo" },
+];
 
 function GroupMemberPicker({
   tag,
@@ -29,28 +37,34 @@ function GroupMemberPicker({
     onPickerOpened();
   };
   const pressedPicker = (e: KeyboardEvent): void => {
-    if (e.key === 'Enter' || e.key === ' ') onPickerOpened();
+    if (e.key === "Enter" || e.key === " ") onPickerOpened();
   };
   const reset = (): void => onTagChange(NONE_TAG_ID);
   // Nodes
   const displayedNode = (isDefault: boolean): ReactElement => {
     const { name, color, classId } = getTag(tag);
-    const style = isDefault ? { background: NONE_TAG.color } : { background: color };
-    const internal = isDefault ? (
-      <>
-        <span className={styles.TagDisplay}>
-          <SamwiseIcon iconName="user-plus" className={styles.CenterIcon} />
-          &nbsp;assign to&nbsp;&nbsp;
-        </span>
-      </>
-    ) : (
-      <>
-        <span className={styles.TagDisplay}>{classId != null ? name.split(':')[0] : name}</span>
-        <button type="button" className={styles.ResetButton} onClick={reset}>
-          &times;
-        </button>
-      </>
-    );
+    const style = isDefault
+      ? { background: NONE_TAG.color }
+      : { background: color };
+    const internal = isDefault
+      ? (
+        <>
+          <span className={styles.TagDisplay}>
+            <SamwiseIcon iconName="user-plus" className={styles.CenterIcon} />
+            &nbsp;assign to&nbsp;&nbsp;
+          </span>
+        </>
+      )
+      : (
+        <>
+          <span className={styles.TagDisplay}>
+            {classId != null ? name.split(":")[0] : name}
+          </span>
+          <button type="button" className={styles.ResetButton} onClick={reset}>
+            &times;
+          </button>
+        </>
+      );
     return (
       <span
         role="presentation"
@@ -67,8 +81,9 @@ function GroupMemberPicker({
     <div className={styles.Main}>
       {displayedNode(tag === NONE_TAG_ID)}
       {opened && (
-        <div className={styles.NewTaskClassPick}>
-          <TagListPicker onTagChange={onTagChange} />
+        // <div className={styles.NewTaskClassPick}>
+        <div>
+          <SearchGroupMember members={members} />
         </div>
       )}
     </div>
