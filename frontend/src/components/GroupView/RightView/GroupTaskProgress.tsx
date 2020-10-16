532e7771a5c8730ca2f5de26ab5cc404700529cb
Jason Tung <jt568@cornell.edu>
2020-10-15 23:30:55 -0400

    css for countdown to deadline

--- frontend/src/components/GroupView/RightView/GroupTaskProgress.tsx
+++ frontend/src/components/GroupView/RightView/GroupTaskProgress.tsx
@@ -11,16 +11,17 @@ const GroupTaskProgress = ({ tasks, deadline }: Props): ReactElement => {
   const tasksDone: number = tasks.filter((task: Task): boolean => task.complete).length;
   const totalTasks: number = tasks.length;
   const oneDay: number = 1000 * 60 * 60 * 24;
-  const deadlineDate: Date = deadline.toDate();
   // Always round down the number of days left. i.e. if the number of hours
   // left is <24 just say there are 0 days left
   const daysLeft: number = Math.floor(
-    Math.max((deadlineDate.getTime() - new Date().getTime()) / oneDay, 0)
+    Math.max((deadline.getTime() - new Date().getTime()) / oneDay, 0)
   );
   return (
     <div>
       <div className={styles.DaysUntilDeadline}>
-        {daysLeft} day{daysLeft === 1 ? '' : 's'} before deadline
+        {daysLeft} day{daysLeft === 1 ? '' : 's'}
+        <br />
+        <span className={styles.BeforeDeadline}>before deadline</span>
       </div>
     </div>
   );
