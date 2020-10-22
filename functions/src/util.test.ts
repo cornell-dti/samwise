a16caea621b88b06a2609a21eb0f4e54d2ca798e
Peter Wu <peterlingwu@gmail.com>
2020-10-22 13:16:08 -0400

    Add unit tests for partition on genericized type

--- functions/src/util.test.ts
+++ functions/src/util.test.ts
@@ -1,5 +1,7 @@
 import { partition } from './util';
 
+type ArbitraryTestingType = { a: string; b: number };
+
 it('partition works', () => {
   expect(partition(['1', '2', '3', '4', '5', '6'], 2)).toEqual([
     ['1', '2'],
@@ -15,4 +17,22 @@ it('partition works', () => {
     ['1', '2', '3'],
     ['4', '5'],
   ]);
+  // test for different types
+  expect(partition([1, 3, 5, 7, 9], 3)).toEqual([
+    [1, 3, 5],
+    [7, 9],
+  ]);
+  const obj1: ArbitraryTestingType = {
+    a: 'a',
+    b: 1,
+  };
+  const obj2: ArbitraryTestingType = {
+    a: 'b',
+    b: 2,
+  };
+  const obj3: ArbitraryTestingType = {
+    a: 'c',
+    b: 3,
+  };
+  expect(partition<ArbitraryTestingType>([obj1, obj2, obj3], 2)).toEqual([[obj1, obj2], [obj3]]);
 });
