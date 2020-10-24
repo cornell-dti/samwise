80b14aad408e13e3f03fb8a96c699fb15fee977d
Pinxuan Huang <48143149+pihucode@users.noreply.github.com>
2020-10-24 15:37:58 -0400

    Completed group tasks (#599)
    * Create section for completed group tasks
    
    * Correct styling for completed group tasks
    
    * Add title above completed tasks section
    
    * Conditionally render completed task section
    
    * Add ids to rendered task list
    
    * Format CompletedTask files
    
    * Move completed tasks section inside group tasks container
    
    * Run prettier
    
    * Re-add memberName

---
+++ frontend/src/components/GroupView/RightView/CompletedTasks/CompletedTask.tsx
@@ -0,0 +1,37 @@
+import { Task } from 'common/types/store-types';
+import React, { ReactElement } from 'react';
+import styles from './CompletedTasks.module.scss';
+import SamwiseIcon from '../../../UI/SamwiseIcon';
+
+type Props = {
+  readonly original: Task;
+  readonly completedTaskCount: number;
+  readonly overflow: boolean;
+};
+
+function CompletedTask({ original, completedTaskCount, overflow }: Props): ReactElement {
+  const month =
+    original.metadata.date instanceof Date ? original.metadata.date.getMonth() + 1 : null;
+  const day = original.metadata.date instanceof Date ? original.metadata.date.getDate() : null;
+  const numericDate = `${month}/${day}`;
+
+  const taskBar = (): ReactElement => (
+    <li className={styles.CompletedTask}>
+      <span>
+        <SamwiseIcon iconName="grabber" className={styles.GrabberIcon} />
+        <span className={styles.CompletedTaskName}>{original.name}</span>
+      </span>
+      <span className={styles.CompletedTaskDate}>{numericDate}</span>
+    </li>
+  );
+
+  const additionalTaskBar = (): ReactElement => (
+    <li className={styles.AdditionalCompletedTask}>
+      {`+${completedTaskCount - 2} completed tasks`}
+    </li>
+  );
+
+  return overflow ? additionalTaskBar() : taskBar();
+}
+
+export default CompletedTask;
