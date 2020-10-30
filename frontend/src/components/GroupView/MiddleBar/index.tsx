c76c643beeab4a87559e01c886e47485edbc2fa2
Ishika Jain <ij36@cornell.edu>
2020-10-30 17:47:01 -0400

    tasks now show up in task queue in group view

--- frontend/src/components/GroupView/MiddleBar/index.tsx
+++ frontend/src/components/GroupView/MiddleBar/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { Group, SamwiseUserProfile } from 'common/types/store-types';
+import { Group, SamwiseUserProfile, Task } from 'common/types/store-types';
 
 import GroupViewMiddleBarPeopleList from './GroupViewMiddleBarPeopleList';
 import GroupViewMiddleBarTaskQueue from './GroupViewMiddleBarTaskQueue';
@@ -7,19 +7,37 @@ import styles from './index.module.scss';
 
 type Props = {
   readonly group: Group;
-  readonly groupMemberProfiles: readonly SamwiseUserProfile[];
+  readonly groupMemberProfiles: SamwiseUserProfile[];
   readonly changeView: (selectedGroup: string | undefined) => void;
+  readonly tasks: readonly Task[];
 };
 
-const GroupViewMiddleBar = ({ groupMemberProfiles, group, changeView }: Props): ReactElement => (
-  <div className={styles.MiddleBar}>
-    <GroupViewMiddleBarTaskQueue />
-    <GroupViewMiddleBarPeopleList
-      group={group}
-      groupMemberProfiles={groupMemberProfiles}
-      changeView={changeView}
-    />
-  </div>
-);
+const GroupViewMiddleBar = ({
+  groupMemberProfiles,
+  group,
+  changeView,
+  tasks,
+}: Props): ReactElement => {
+  const allInfo: any = localStorage.getItem('firebaseui::rememberedAccounts');
+  const allInfoJSON = JSON.parse(allInfo);
+  const { email } = allInfoJSON[0];
+  return (
+    <div>
+      {groupMemberProfiles.map(() => {
+        const filteredTasks = tasks.filter(({ owner }) => owner.includes(email));
+        return (
+          <div className={styles.MiddleBar}>
+            <GroupViewMiddleBarTaskQueue tasks={filteredTasks} />
+            <GroupViewMiddleBarPeopleList
+              group={group}
+              groupMemberProfiles={groupMemberProfiles}
+              changeView={changeView}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default GroupViewMiddleBar;
