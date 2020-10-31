f40af35b2894be8cc682134b7c5a366f7ca2ff0d
IshikaJain <ishikajain332@gmail.com>
2020-10-31 15:47:10 -0400

    Tasks now show up in task queue in group view (#622)
    * tasks now show up in task queue in group view
    
    * Used getAppUser() and incorporated some of the other suggestions Sam made
    
    * Took out unecessary important from GroupViewMiddleBarTaskQueue.scss

--- frontend/src/components/GroupView/MiddleBar/GroupViewMiddleBarTaskQueue.tsx
+++ frontend/src/components/GroupView/MiddleBar/GroupViewMiddleBarTaskQueue.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { ReactNode, ReactElement } from 'react';
+
+import { Task } from 'common/types/store-types';
 import styles from './GroupViewMiddleBarTaskQueue.module.scss';
+import GroupTask from '../RightView/GroupTask';
+
+type Props = {
+  readonly tasks: readonly Task[];
+};
 
 const EmptyTaskQueue = (): React.ReactElement => (
   <div className={styles.EmptyTaskQueue}>
@@ -7,11 +14,22 @@ const EmptyTaskQueue = (): React.ReactElement => (
     <button type="button">Add task</button>
   </div>
 );
-
-const TaskQueue = (): React.ReactElement => (
-  <div className={styles.TaskQueue}>
-    <h2>Task Queue</h2>
-    <EmptyTaskQueue />
+function renderTaskList(tasks: readonly Task[]): ReactNode {
+  return tasks.map((task) => <GroupTask key={task.id} original={task} memberName="" />);
+}
+const TaskQueue = ({ tasks }: Props): React.ReactElement => (
+  <div>
+    {tasks.length > 0 ? (
+      <div className={styles.Container}>
+        <h2>Task Queue</h2>
+        {renderTaskList(tasks)}
+      </div>
+    ) : (
+      <div className={styles.TaskQueue}>
+        <h2>Task Queue</h2>
+        <EmptyTaskQueue />
+      </div>
+    )}
   </div>
 );
 
