d8b344c3b6291026c457d6711e91941cf96a6ee2
Peter Wu <peterlingwu@gmail.com>
2020-11-08 21:37:15 -0500

    Firestore Task <- Subtask merge function (step 1) + fix DB credential (#623)
    * Add merge subtask with task script for backend
    
    * Add immutable as a dependency in functions
    
    * Update map for immutability
    
    * Fix script to be independent of frontend PR
    
    * Clean up code
    
    * Fix order consistency in merge-task-subtask
    
    * Fix type errors after merging in #635
    
    * Fix db credentials property

---
+++ functions/src/merge-task-subtask.ts
@@ -0,0 +1,83 @@
+import { FirestoreCommon } from 'common/types/firestore-types';
+import { Map } from 'immutable';
+import { partition } from './util';
+import database from './db';
+
+type OldFirestoreTask = FirestoreCommon & {
+  readonly name: string;
+  readonly tag: string;
+  readonly complete: boolean;
+  readonly inFocus: boolean;
+  readonly children: readonly string[];
+};
+
+type OldFirestoreSubTask = {
+  complete: boolean;
+  inFocus: boolean;
+  name: string;
+  order: number;
+  owner: string;
+};
+
+type NewSubTask = Omit<OldFirestoreSubTask, 'owner'>;
+
+type TaskUpdateData = { id: string; subtasks: NewSubTask[] };
+
+const mergeSubtaskTask = async (): Promise<void> => {
+  const tasksSnapshot = await database.tasksCollection().get();
+  const subTasksSnapshot = await database.subTasksCollection().get();
+  // Initialize map from subtask ID to FirestoreCommonTask representing its data
+  let subTaskMap: Map<string, OldFirestoreSubTask> = Map();
+
+  // Populate map for constant time retrieval of subtasks by ID
+  subTasksSnapshot.forEach((document) => {
+    const data = document.data();
+    const subTask = data as OldFirestoreSubTask;
+    subTaskMap = subTaskMap.set(document.id, subTask);
+  });
+
+  const idListToUpdate: TaskUpdateData[] = [];
+  tasksSnapshot.docs.forEach((document) => {
+    const { id } = document;
+    const data = document.data();
+    if (data != null) {
+      const task = data as OldFirestoreTask;
+      const { children } = task;
+      const subtasks: NewSubTask[] = [];
+      if (children !== undefined && children.length !== 0) {
+        const subTaskOrderSet: Set<number> = new Set<number>();
+        children.forEach((subtaskId) => {
+          const subtask: NewSubTask | undefined = subTaskMap.get(subtaskId);
+          if (subtask !== undefined) {
+            const { complete, inFocus, name, order } = subtask;
+            if (subTaskOrderSet.has(order)) {
+              const uniqueOrder =
+                [...subTaskOrderSet].reduce((acc, curr) => Math.max(acc, curr), 0) + 1;
+              subTaskOrderSet.add(uniqueOrder);
+              subtasks.push({ complete, inFocus, name, order: uniqueOrder });
+            } else {
+              subTaskOrderSet.add(order);
+              subtasks.push({ complete, inFocus, name, order });
+            }
+          }
+        });
+      }
+      idListToUpdate.push({ id, subtasks });
+    }
+  });
+  // Used to overcome to the 500 item per batch limit.
+  const partitioned = partition<TaskUpdateData>(idListToUpdate, 500);
+  // eslint-disable-next-line no-restricted-syntax
+  for (const idOwnerList of partitioned) {
+    const batch = database.db().batch();
+    idOwnerList.forEach(({ id, subtasks }) =>
+      batch.update(database.tasksCollection().doc(id), {
+        children: subtasks,
+      })
+    );
+    // eslint-disable-next-line no-await-in-loop
+    await batch.commit();
+  }
+};
+
+export default mergeSubtaskTask;
