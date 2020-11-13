9896563132a141304015818310ce5fe6d6a14f4d
Peter Wu <peterlingwu@gmail.com>
2020-11-12 22:00:53 -0500

    Fix FloatingTaskEditor checkbox and focus

--- frontend/src/components/Util/TaskEditors/FloatingTaskEditor.tsx
+++ frontend/src/components/Util/TaskEditors/FloatingTaskEditor.tsx
@@ -127,7 +127,6 @@ export default function FloatingTaskEditor({
             className={styles.Editor}
             editorRef={editorRef}
             calendarPosition={calendarPosition}
-            active
           />
           <div className={styles.BackgroundBlocker} role="presentation" onClick={closePopup} />
         </>
