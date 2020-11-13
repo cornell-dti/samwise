17d442bf6ed0a445b210c421370ba64e336014b4
Jason Tung <jt568@cornell.edu>
2020-11-13 03:24:09 -0500

    unused imports

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,8 @@
 import * as functions from 'firebase-functions';
-import sgMail from '@sendgrid/mail';
 import getICalLink from './iCalFunctions';
 import focusTasksDueToday from './focus-today-task';
 import removeOldTasks from './remove-old-tasks';
-import sendgridKey from './sendgrid-key.json';
 import sendEmail from './send-email';
-
-sgMail.setApiKey(sendgridKey.SENDGRID_API_KEY);
 
 export const iCalFunction = functions.pubsub.schedule('0 0 * * *').onRun(getICalLink);
 
