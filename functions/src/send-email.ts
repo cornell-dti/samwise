2e88349b22ebb46dfee5ada1311b284ad8e71b8a
Jason Tung <jt568@cornell.edu>
2020-11-16 14:33:42 -0500

    Send email notification (#649)
    * export http function
    
    * email function and start frontend impl
    
    * change onHttpRequest to onCall
    
    * fix some warnings
    
    * check where error is coming from
    
    * fix errors to start pr
    
    * remove test data
    
    * finish implemeneting call to send email on frontend
    
    * unused imports
    
    * Create sendgrid-key.json
    
    * fix imports and package.json
    
    * Delete sendgrid-key.json
    
    * Create sendgrid-key.json
    
    * remove functions gitignore
    
    * create dummy file for sendgrid key rather than checking in file
    
    * fix syntax on ci-functions
    
    * fix quotes on ci
    
    * Delete sendgrid-key.json
    
    * change bell to reminder in email
    
    * add dummy sendgrid key to ci
    
    * fix dummy files on ci again
    
    * Use real sendgrid api json in deployment jobs
    
    Co-authored-by: Sam <sam@developersam.com>

---
+++ functions/src/send-email.ts
@@ -0,0 +1,68 @@
+import * as functions from 'firebase-functions';
+import sgMail from '@sendgrid/mail';
+import { FirestoreGroup, FirestoreUserData } from 'common/types/firestore-types';
+import db from 'db';
+import sendgridKey from './sendgrid-key.json';
+
+sgMail.setApiKey(sendgridKey.SENDGRID_API_KEY);
+
+type Data = {
+  data: {
+    variant: string;
+    recipientEmail: string;
+    groupId: string;
+  };
+};
+
+const sendEmail = async (data: Data, context: functions.https.CallableContext): Promise<string> => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      'failed-precondition',
+      'The function must be called while authenticated.'
+    );
+  }
+  const { email } = context.auth.token;
+  const { recipientEmail, groupId } = data.data;
+  let { variant } = data.data;
+
+  // default variant
+  if (!['hug', 'reminder'].includes(variant)) {
+    variant = 'hug';
+  }
+  const recipient: FirestoreUserData = (
+    await db.usersCollection().doc(recipientEmail)?.get()
+  ).data() as FirestoreUserData;
+  const sender: FirestoreUserData = (
+    await db.usersCollection().doc(email)?.get()
+  ).data() as FirestoreUserData;
+  const group: FirestoreGroup = (
+    await db.groupsCollection().doc(groupId)?.get()
+  ).data() as FirestoreGroup;
+
+  const recipientName = recipient.name.split(' ')[0];
+  const senderName = sender.name.split(' ')[0];
+  const msg = {
+    to: recipientEmail,
+    from: 'jt568@cornell.edu',
+    subject: `You got a ${variant}!`,
+    text: `${senderName} from ${group.name} is sending you a ${variant}!`,
+    html: `${senderName} from ${group.name} is sending you a ${variant}!`,
+  };
+
+  if (group && group.members.includes(email as string) && group.members.includes(recipientEmail)) {
+    return sgMail
+      .send(msg)
+      .then(() => {
+        return 'sent';
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error(error);
+        return `${error} error in sending ${variant} from ${senderName} to ${recipientName}`;
+      });
+  }
+
+  return 'the members are not all in the correct group';
+};
+
+export default sendEmail;
