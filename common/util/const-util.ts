724360ec26b88ba9da349ba2993cf175c02c2ed1
Megan Yin <my474@cornell.edu>
2020-12-21 20:18:44 -0500

    oops 2021

--- common/util/const-util.ts
+++ common/util/const-util.ts
@@ -1,9 +1,9 @@
 /**
  * The (exclusive) last day of class for Fall 2020
  */
-export const LAST_DAY_OF_CLASS = new Date('05/15/2020');
+export const LAST_DAY_OF_CLASS = new Date('05/15/2021');
 
 /**
  * The (exclusive) last day of final exams for Fall 2020
  */
-export const LAST_DAY_OF_EXAMS = new Date('05/26/2020');
+export const LAST_DAY_OF_EXAMS = new Date('05/26/2021');
