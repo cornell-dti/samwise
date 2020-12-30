08cf85fd1ed1d192179c256846e054fa0f14ca79
Sam Zhou <sam@developersam.com>
2020-12-29 22:36:21 -0500

    Support editing group deadline (#724)

--- frontend/src/components/GroupView/RightView/index.tsx
+++ frontend/src/components/GroupView/RightView/index.tsx
@@ -1,5 +1,6 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import type { Group, SamwiseUserProfile, Task } from 'common/types/store-types';
+import Calendar from 'react-calendar';
 import SamwiseIcon from '../../UI/SamwiseIcon';
 import GroupTaskRow from './GroupTaskRow';
 import styles from './index.module.scss';
@@ -16,6 +17,7 @@ type Props = {
 
 const RightView = ({ group, groupMemberProfiles, tasks }: Props): ReactElement => {
   const setTaskCreatorContext = useTaskCreatorContextSetter();
+  const [showDateEditor, setShowDateEditor] = useState(false);
 
   const onEditGroupNameClicked = (): void => {
     promptTextInput('Edit your group name', '', 'New Group Name', 'Submit', 'text').then((name) =>
@@ -36,13 +38,34 @@ const RightView = ({ group, groupMemberProfiles, tasks }: Props): ReactElement =
       </div>
 
       <div className={styles.RightViewMain}>
-        <div>
-          <h2>{group.name}</h2>
+        <div className={styles.TitleRow}>
+          <h2 className={styles.Title}>{group.name}</h2>
           <SamwiseIcon
             iconName="pencil"
             className={styles.EditGroupNameIcon}
             onClick={onEditGroupNameClicked}
           />
+          <span className={styles.DeadlineText}>
+            {`Deadline: ${group.deadline.getMonth() + 1}/${group.deadline.getDate()}`}
+          </span>
+          <SamwiseIcon
+            iconName="calendar-dark"
+            className={styles.EditGroupDeadlineIcon}
+            onClick={() => setShowDateEditor((shown) => !shown)}
+          />
+          {showDateEditor && (
+            <Calendar
+              value={group.deadline}
+              className={styles.DeadlineCalendarPicker}
+              minDate={new Date()}
+              onChange={(date) => {
+                if (!(date instanceof Date)) throw new Error();
+                updateGroup({ ...group, deadline: date });
+                setShowDateEditor(false);
+              }}
+              calendarType="US"
+            />
+          )}
         </div>
         <div className={styles.GroupTaskRowContainer}>
           {groupMemberProfiles.map((user) => {
