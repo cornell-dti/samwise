de25f9e84df50f6933b84f742cbc15c7e031a5f2
Peter Wu <peterlingwu@gmail.com>
2021-02-09 16:30:00 -0500

    Update course-info fetch script for SP21 and eliminate semifinals (#730)

--- course-info/src/main.ts
+++ course-info/src/main.ts
@@ -1,20 +1,19 @@
 import { writeFileSync } from 'fs';
 
 import getAllCoursesInSemester from './fetch-courses';
-import { createFinalJson, createPrelimJson, createSemiFinalJson } from './fetch-exam';
+import { createPrelimJson } from './fetch-exam';
 import mergeCoursesAndExamJson from './merge-json';
 
-const SEMESTER = 'FA20';
+const SEMESTER = 'SP21';
 const JSON_FILENAME = 'courses-with-exams-min.json';
 
 async function main(): Promise<void> {
-  const [courses, prelimExams, semifinalExams, finalExams] = await Promise.all([
+  const [courses, prelimExams] = await Promise.all([
     getAllCoursesInSemester(SEMESTER),
     createPrelimJson(),
-    createSemiFinalJson(),
-    createFinalJson(),
+    // createFinalJson(),
   ]);
-  const json = mergeCoursesAndExamJson(courses, prelimExams, semifinalExams, finalExams);
+  const json = mergeCoursesAndExamJson(courses, prelimExams);
   // eslint-disable-next-line no-console
   writeFileSync(JSON_FILENAME, JSON.stringify(json));
 }
